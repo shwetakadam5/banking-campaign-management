6ed3ea77730bda1f19921d7b66e64f5ce87bc822
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-07-20 14:54:51 +1000

    Added the router related changes in the main.jsx of the client.

--- bcm-client/src/main.jsx
+++ bcm-client/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Adding the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import "./index.css";
+
+// All the accessible routes, and which components respond to which URL are defined
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
