986640057dbb3178af81e92fa2e2d8dc23b3de7b
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-07-23 17:55:53 +1000

    Deleted all default styling on the pages.

--- bcm-client/src/main.jsx
+++ bcm-client/src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // Adding the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
-import "./index.css";
+import Login from "./pages/Login.jsx";
 
 // All the accessible routes, and which components respond to which URL are defined
 const router = createBrowserRouter([
@@ -18,12 +19,18 @@ const router = createBrowserRouter([
         index: true,
         element: <Home />,
       },
+      {
+        path: "/login",
+        element: <Login />,
+      },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 );
